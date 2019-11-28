683c0f00f29805fb021f8e7456e339ba355749e7
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-11-28 10:54:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ end
 
 
 def add_element_to_start_of_array(array, element)
-  return ["I", "am", "really", "learning"].unshift(element)
+  return array.unshift(element)
   
 end
 ["I", "am", "really", "learning"].unshift("wow")
